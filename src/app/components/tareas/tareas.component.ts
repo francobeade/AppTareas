39fab60df21b24b6e3b29f92d5545dfcdfe2dc24
Franco Ismael Beade <francobeade97@gmail.com>
2021-10-24 22:21:34 -0300

    Subida de la app

---
+++ src/app/components/tareas/tareas.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Tarea } from 'src/app/models/tarea';
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent implements OnInit {
+  listTareas: Tarea[] = []
+  nombreTarea = ''
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  agregarTarea() {
+
+    //Crear un objeto tarea
+    const tarea: Tarea = {
+      nombre: this.nombreTarea,
+      estado: false
+    }
+
+    //Agregar el objeto tarea al array
+    this.listTareas.push(tarea);
+
+    //Reset form
+    this.nombreTarea = '';
+  }
+
+
+  eliminarTarea(index: number): void {
+    this.listTareas.splice(index, 1);
+  }
+
+  actualizarTarea(tarea: Tarea, index: number): void {
+    this.listTareas[index].estado = !tarea.estado;
+  }
+}
